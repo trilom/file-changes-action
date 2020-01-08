42be13c99c5ebd09ef8a1edc5dd8438be4c0106c
Bryan Killian <carnoco@gmail.com>
2020-01-08 02:11:10 -0500

    prod dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,99 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+// External Dependencies
+import * as core from "@actions/core"
+import * as fs from "fs"
+import * as gh from "@actions/github"
 
+class ChangedFiles {
+  updated: Array<string> = []
+  created: Array<string> = []
+  deleted: Array<string> = []
+  files: Array<string> = []
+}
+
+class File {
+  added: string = ''
+  modified: string = ''
+  removed: string = ''
+  filename: string = ''
+  status: string = ''
+  previous_filename: string = ''
+}
+
+async function sortChangedFiles(files: any): Promise<ChangedFiles> {
+  return files.reduce((acc: ChangedFiles, f: File) => {
+    if (f.status === "added" || f.added) {
+      acc.created.push(f.filename === undefined ? f.added : f.filename)
+      acc.files.push(f.filename === undefined ? f.added : f.filename)
+    }
+    if (f.status === "removed" || f.removed) {
+      acc.deleted.push(f.filename === undefined ? f.removed : f.filename)
+    }
+    if (f.status === "modified" || f.modified) {
+      acc.updated.push(f.filename === undefined ? f.modified : f.filename)
+      acc.files.push(f.filename === undefined ? f.modified : f.filename)
+    }
+    if (f.status === "renamed") {
+      acc.created.push(f.filename)
+      acc.deleted.push(f.previous_filename)
+    }
+    return acc
+  }, new ChangedFiles())
+}
+
+async function getChangedPRFiles(client: gh.GitHub, prNumber: number): Promise<ChangedFiles> {
+  const response = await client.pulls.listFiles({
+    owner: gh.context.repo.owner,
+    repo: gh.context.repo.repo,
+    pull_number: prNumber,
+  })
+  return sortChangedFiles(response.data)
+}
+
+async function getChangedPushFiles(commits: Array<any>): Promise<ChangedFiles> {
+  const distinctCommits = commits.filter(c => c.distinct)
+  return sortChangedFiles(distinctCommits)
+}
+
+function getPrNumber(): number | null {
+  const pr = gh.context.payload.pull_request
+  return pr ? pr.number : null
+}
+
+// figure out if it is a PR or Push
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const token: string = core.getInput("repo-token")
+    const github: any = core.getInput("github-object")
+    const client = new gh.GitHub(token)
+    let changedFiles = new ChangedFiles()
+    if (github.event_name == "push") {
+      // do push actions
+      changedFiles = await getChangedPushFiles(github.event.commits)
+    } else if (github.event_name == "pullRequest") {
+      // do PR actions
+      const prNumber = getPrNumber()
+      if (prNumber != null) {
+        changedFiles = await getChangedPRFiles(client, prNumber)
+      } else {
+        core.setFailed("Could not get pull request number from context, exiting")
+        return
+      }
+    } else {
+      core.setFailed(`Change not initiated by a PR or Push, it was ${github.event_name} instead.`)
+      return
+    }
+    //write files to preserve original functionality
+    fs.writeFileSync(`${process.env.HOME}/files.json`, JSON.stringify(changedFiles.files), "utf-8")
+    fs.writeFileSync(`${process.env.HOME}/files_modified.json`, JSON.stringify(changedFiles.updated), "utf-8")
+    fs.writeFileSync(`${process.env.HOME}/files_added.json`, JSON.stringify(changedFiles.created), "utf-8")
+    fs.writeFileSync(`${process.env.HOME}/files_deleted.json`, JSON.stringify(changedFiles.deleted), "utf-8")
+    //also export some outputs
+    core.setOutput("files_created", changedFiles.created.join(" "))
+    core.setOutput("files_updated", changedFiles.updated.join(" "))
+    core.setOutput("files_deleted", changedFiles.deleted.join(" "))
+    process.exit(0)
   } catch (error) {
+    core.error(error)
     core.setFailed(error.message)
   }
 }
