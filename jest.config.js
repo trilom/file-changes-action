a6d1a98dee07630f6559d1561a237839e80b00f2
Bryan Killian <carnoco@gmail.com>
2020-03-08 07:46:23 -0400

    chore(cleaning): buncha cleaning

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
   testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
+  testMatch: ['<rootDir>/__tests__/*.test.ts'],
   testRunner: 'jest-circus/runner',
   transform: {
     '^.+\\.ts$': 'ts-jest'
