802723df40154267d7f208e8bf41de55b5c03f1c
Bryan Killian <carnoco@gmail.com>
2020-03-19 07:06:55 -0400

    test(codecov): set up codecov.yml hopefully

--- jest.config.js
+++ jest.config.js
@@ -8,9 +8,21 @@ module.exports = {
   coverageThreshold: {
     global: {
       branches: 50,
-      functions: 80,
+      functions: 70,
       lines: 75,
       statements: 75
+    },
+    './src/*.ts': {
+      branches: 70,
+      functions: 85,
+      lines: 85,
+      statements: 85
+    },
+    './src/tests/**/*.ts': {
+      branches: 50,
+      functions: 60,
+      lines: 65,
+      statements: 65
     }
   }
 }
